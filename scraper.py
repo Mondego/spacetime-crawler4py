bc3a5eaf61f7fe772d6a232fa17ccf40f335b781
Rohan Achar <ra.rohan@gmail.com>
2019-10-09 13:18:19 -0700

    Adding the is_valid function.

--- scraper.py
+++ scraper.py
@@ -1,3 +1,25 @@
+import re
+from urllib.parse import urlparse
 
 def scraper(url, resp):
     return list()
+
+
+def is_valid(url):
+    try:
+        parsed = urlparse(url)
+        if parsed.scheme not in set(["http", "https"]):
+            return False
+        return not re.match(
+            r".*\.(css|js|bmp|gif|jpe?g|ico"
+            + r"|png|tiff?|mid|mp2|mp3|mp4"
+            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+            + r"|epub|dll|cnf|tgz|sha1"
+            + r"|thmx|mso|arff|rtf|jar|csv"
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+
+    except TypeError:
+        print ("TypeError for ", parsed)
+        raise
