ad351308abee6796a5a6f7388e8475317578015a
Jason <jasoz10@uci.edu>
2020-02-07 14:41:46 -0800

    Testing, please revert if broke

--- scraper.py
+++ scraper.py
@@ -28,8 +28,8 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        '''
-        return not re.match(
+
+        if(not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
@@ -37,8 +37,15 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()) '''
-        return True
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())):
+                return False
+        if(re.match(
+            r"*.ics.uci.edu/*|*.cs.uci.edu/*|*.informatics.uci.edu/*|*.stat.uci.edu/*|today.uci.edu/department/information_computer_sciences/*"
+            ,parsed.netloc.lower() )):
+                return True    
+    
+        return False
+
     except TypeError:
         print ("TypeError for ", parsed)
         raise
