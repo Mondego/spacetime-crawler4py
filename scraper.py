41f1611054c54916568f59c92ba5461be5691973
Shireen Hsu <shireen0718@gmail.com>
2020-02-07 16:36:23 -0800

    fixed regex for reals this time

--- scraper.py
+++ scraper.py
@@ -29,7 +29,7 @@ def is_valid(url):
         if parsed.scheme not in set(["http", "https"]):
             return False
 
-        if(not re.match(
+        if( re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
@@ -39,11 +39,13 @@ def is_valid(url):
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())):
                 return False
+
+        #print(parsed.netloc.lower())
         if(re.match(
-            r".*\.ics\.uci\.edu/.*|.*\.cs\.uci\.edu/.*|.*\.informatics\.uci\.edu/.*|.*\.stat\.uci\.edu/.*"
-            + r"|today\.uci\.edu/department/information_computer_sciences/.*$"
+            r".*\.ics\.uci\.edu\/?.*|.*\.cs\.uci\.edu\/?.*|.*\.informatics\.uci\.edu\/?.*|.*\.stat\.uci\.edu\/?.*"
+            + r"|today\.uci\.edu\/department\/information_computer_sciences\/?.*$"
             ,parsed.netloc.lower() )):
-                return True    
+            return True    
     
         return False
 
