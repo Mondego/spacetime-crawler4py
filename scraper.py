66b86788d3c712c825268792ef64b7e11dd8aa87
Shireen Hsu <shireen0718@gmail.com>
2020-02-09 11:43:06 -0800

    add a list of possbile traps

--- scraper.py
+++ scraper.py
@@ -40,7 +40,7 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())):
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())): # need to check if other parts have these toos
                 return False
 
         # print(parsed.netloc.lower())
