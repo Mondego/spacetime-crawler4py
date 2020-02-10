7dd25dec3c717291d3b51b20721338add331a9dd
Jason <jasoz10@uci.edu>
2020-02-09 21:57:41 -0800

    logging
    plz

--- scraper.py
+++ scraper.py
@@ -68,10 +68,12 @@ def is_valid(url):
 
         parser = urllib.robotparser.RobotFileParser()
         parser.set_url(url)
+        
         parser.read()
         if(parser.can_fetch("IR W20 94612036 73401826 79557971",url)):
+            self.logger("Roboted successfully")
             return True
-            
+
         return False
 
     except TypeError:
