dd5068c1959c0ff86c4c95b810426efbec6f37a7
Scott Buchmiller <scottbuchmiller@gmail.com>
2020-02-10 00:44:38 -0800

    fixed scraper in frontier

--- crawler/frontier.py
+++ crawler/frontier.py
@@ -5,13 +5,14 @@ from threading import Thread, RLock
 from queue import Queue, Empty
 
 from utils import get_logger, get_urlhash, normalize
-from scraper import is_valid
+from scraper import Scrape
 
 class Frontier(object):
     def __init__(self, config, restart):
         self.logger = get_logger("FRONTIER")
         self.config = config
         self.to_be_downloaded = list()
+        self.s = Scrape()
         
         if not os.path.exists(self.config.save_file) and not restart:
             # Save file does not exist, but request to load save.
@@ -40,7 +41,7 @@ class Frontier(object):
         total_count = len(self.save)
         tbd_count = 0
         for url, completed in self.save.values():
-            if not completed and is_valid(url):
+            if not completed and self.s.is_valid(url):
                 self.to_be_downloaded.append(url)
                 tbd_count += 1
         self.logger.info(
