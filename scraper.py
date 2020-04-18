d37751f642d3f03942f39c2e768d83381b8ef8ca
Adrienne <deniseaz@uci.edu>
2020-04-18 15:10:09 -0700

    added to is_valid
    checks url for
    *.ics.uci.edu/*
    *.cs.uci.edu/*
    *.informatics.uci.edu/*
    *.stat.uci.edu/*
    today.uci.edu/department/information_computer_sciences/*

--- scraper.py
+++ scraper.py
@@ -12,9 +12,11 @@ def extract_next_links(url, resp):
 def is_valid(url):
     try:
         parsed = urlparse(url)
+        
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
+        
+        checkext = not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
@@ -23,6 +25,19 @@ def is_valid(url):
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+        
+        # check if domain/path matches requirements
+        checkdomain = (re.match(r"(www.)?[-a-z0-9.]*\.ics\.uci\.edu", parsed.hostname) or \
+            re.match(r"(www.)?[-a-z0-9.]*\.cs\.uci\.edu", parsed.hostname) or \
+            re.match(r"(www.)?[-a-z0-9.]*\.informatics\.uci\.edu", parsed.hostname) or \
+            re.match(r"(www.)?[-a-z0-9]*\.stat\.uci\.edu", parsed.hostname) or \
+            (re.match(r"(www.)?today\.uci\.edu", parsed.hostname) and \
+            (re.match(r"\/department\/information_computer_sciences\/.*", parsed.path))))
+        
+        if checkext and checkdomain:
+            return True
+        else:
+            return False
 
     except TypeError:
         print ("TypeError for ", parsed)
