5ea362271b95818a375560cc6b79b2d0c188c3b6
Heon Jae Jeong <ericj1114@gmail.com>
2020-04-21 20:35:55 -0700

    add ppsx and odc to invalid files

--- scraper.py
+++ scraper.py
@@ -57,7 +57,7 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz|ppsx|odc)$", parsed.path.lower())
 
     except TypeError:
         print ("TypeError for ", parsed)
