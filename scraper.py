12c8db7fe28e420b29c29254374120f32720e7f9
David Arico <davidarico@me.com>
2020-11-02 12:49:24 -0800

    Added more logic for the scrapper function

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import re
+from bs4 import BeautifulSoup, SoupStrainer
 from urllib.parse import urlparse
 
 def scraper(url, resp):
@@ -6,14 +7,38 @@ def scraper(url, resp):
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # Implementation requred.
-    return list()
+    return_list = []
+
+    if (resp.raw_response is None):
+        return list()
+
+    # Parse the html but only parse the URLs to make it more effiecent
+    # SoupStrainer code found https://www.crummy.com/software/BeautifulSoup/bs4/doc/#soupstrainer
+    soup = BeautifulSoup(resp.raw_response.content, parse_only=SoupStrainer("a"), features="html.parser")
+    for link in soup:
+        if link.has_attr("href") and is_valid(link["href"]):
+            return_list.append(link["href"])
+
+    print(return_list)
+    return return_list
 
 def is_valid(url):
+    valid_sites = [
+        ".ics.uci.edu",
+        ".cs.uci.edu",
+        ".informatics.uci.edu",
+        ".stat.uci.edu",
+        "today.uci.edu/department/information_computer_sciences"
+    ]
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
+
+        for base_link in valid_sites:
+            if(not (base_link in parsed.netloc)):
+                return False
+        
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
