f15d07efe8b57535ab4e3e0b7fcf914db8a971c9
Nathan Su <40217648+NathanSu0304@users.noreply.github.com>
2021-01-30 16:44:38 -0800

    update

--- scraper.py
+++ scraper.py
@@ -1,62 +1,67 @@
 import re, shelve, urllib
-from collections import defaultdict
-from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+from urllib.request import urlopen
+from utils.response import Response
+from bs4 import BeautifulSoup
+from collections import defaultdict
 
-def scraper(url, resp):
+# global variable for regular expression
+allowed_url = ['.+\.cs.uci.edu/.*', '.+\.ics.uci.edu/.*', '.+\.informatics.uci.edu/.*', '.+\.stat.uci.edu/.*',
+               'today.uci.edu/department/information_computer_sciences/.*']
+allowed_url = [re.compile(x) for x in allowed_url]
+
+
+def scraper(url: str, resp: Response) -> list:
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
+
 def extract_next_links(url, resp):
     links = []
-    if 599 >=resp.status >= 200 and resp.raw_response != None:
-        bsObj = BeautifulSoup(resp.raw_response.content, 'html.parser')
+    # get links from resp
+    if 200 <= resp.status <= 599 and resp.raw_response != None:
+        soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+        # Store text and url to shelve
         try:
-            with shelve.open('store_url') as db:
-                un_url = url.split("#")[0]
-                # get text on the url and write into db
-                text = re.findall(r'^\w+$', bsObj.get_text().strip().lower())
-                print(text)
-                if un_url not in db:
-                    db[un_url] = text
+            url_list = shelve.open('urlText.db')
+            un_url = url.split('#')[0]
 
-                t1 = bsObj.find_all('a')
-                for t2 in t1:
-                    if(t2.get('href') != None and t2.get('href') not in links):
-                        links.append(t2)
-                        print(f"links: {links}")
+            all_word = re.sub(r'[^A-Z^a-z^0-9^ ]', '', soup.get_text().strip().lower())
+
+            if un_url not in url_list:
+                url_list[un_url] = all_word
+
+            for link in soup.findAll('a'):
+                if link.get('href') is not None:
+                    if link.get('href') not in url_list:
+                        links.append(link.get('href'))
 
         finally:
-            db.close()
+            url_list.close()
+
     return links
 
 
-def containHugeNum(path):
-    return not re.match('^\d+$', path)
-
 def is_valid(url):
-    seeds = ['.+\.cs.uci.edu/.*',
-             '.+\.ics.uci.edu/.*',
-             '.+\.informatics.uci.edu/.*',
-             '.+\.stat.uci.edu/.*',
-             'today.uci.edu/department/information_computer_sciences/.*']
-    seeds = [re.compile(i)for i in seeds]
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        if len(parsed.path.split('/')) > 20:
+        elif len(parsed.path.split('/')) > 20:
             return False
-        if not any([i.match(url) for i in seeds]):
-            return False
-        the_path = parsed.path.split('/')
-        path_dict = defaultdict(int)
-
-        for i in the_path:
-            if containHugeNum(i.lower()):
+        else:
+            if not any([i.match(url) for i in allowed_url]):
                 return False
-            path_dict[i] +=1
-            if path_dict[i] >=4:
+
+        The_path = parsed.path.split("/")
+        pass_dict = defaultdict(int)
+
+        for i in The_path:
+            if "pdf" in i or "img" in i or re.match('[0-9]+', i) or re.match('[0-9]+-[0-9]+-[0-9]+', i):
+                return False
+            pass_dict[i] += 1
+
+            if pass_dict[i] > 4:
                 return False
 
         return not re.match(
@@ -67,8 +72,8 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz|ppsx)$", parsed.path.lower())
 
     except TypeError:
-        print ("TypeError for ", parsed)
+        print("TypeError for ", parsed)
         raise
