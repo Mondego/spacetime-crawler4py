57d73b28e00b3f1cc33baa004e97ddae9857e75b
Nathan Su <40217648+NathanSu0304@users.noreply.github.com>
2021-01-30 22:56:04 -0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,47 +1,44 @@
 import re, shelve, urllib
-from collections import defaultdict
-from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+from urllib.request import urlopen
+from utils.response import Response
+from bs4 import BeautifulSoup
+from collections import defaultdict
 
-def scraper(url, resp):
+# global variable for regular expression
+allowed_url = ['.+\.cs.uci.edu/.*', '.+\.ics.uci.edu/.*', '.+\.informatics.uci.edu/.*', '.+\.stat.uci.edu/.*', 'today.uci.edu/department/information_computer_sciences/.*']
+allowed_url = [re.compile(x) for x in allowed_url]
+
+def scraper(url: str, resp: Response) -> list:
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
+
 def extract_next_links(url, resp):
     links = []
-    if 599 >= resp.status >= 200 and resp.raw_response != None:
-        bsObj = BeautifulSoup(resp.raw_response.content, 'html.parser')
+    # get links from resp
+    if 200 <= resp.status <= 599 and resp.raw_response != None:
+        soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+        # Store text and url to shelve
         try:
-            # url_list = shelve.open('urlText.db')
-            # un_url = url.split("#")[0]
-            ## get text on the url and write into db
-            # text = re.findall(r'^\w+$', bsObj.get_text().strip().lower())
-            # if un_url not in url_list:
-            #     url_list[un_url] = text
+            url_list = shelve.open('urlText.db')
+            un_url = url.split('#')[0]
 
-            for link in bsObj.findAll('a'):
+            all_word = re.sub(r'[^A-Z^a-z^0-9^ ]', '', soup.get_text().strip().lower())
+
+            if un_url not in url_list:
+                url_list[un_url] = all_word
+
+            for link in soup.findAll('a'):
                 if link.get('href') is not None:
                     if link.get('href') not in url_list:
-                        links.append(link.get('href'))
-            # t1 = bsObj.find_all('a')
-            # for t2 in t1:
-            #     if(t2.get('href') != None and t2.get('href') not in links):
-            #         links.append(t2)
-
+                        links.append(link.get('href'))  
+                    
         finally:
             url_list.close()
+
     return links
 
-
-def containHugeNum(path):
-    return not re.match('^\d+$', path)
-
-seeds = ['.+\.cs.uci.edu/.*',
-             '.+\.ics.uci.edu/.*',
-             '.+\.informatics.uci.edu/.*',
-             '.+\.stat.uci.edu/.*',
-             'today.uci.edu/department/information_computer_sciences/.*']
-allowed_url = [re.compile(i) for i in seeds]
 def is_valid(url):
     try:
         parsed = urlparse(url)
@@ -57,9 +54,11 @@ def is_valid(url):
         pass_dict = defaultdict(int)
         
         for i in The_path:
+            if "pdf" in i or "img" in i or re.match('[0-9]+', i) or re.match('[0-9]+-[0-9]+-[0-9]+',i):
+                return False
             pass_dict[i] += 1
             
-            if pass_dict[i] > 3:
+            if pass_dict[i] > 4:
                 return False
             
         return not re.match(
