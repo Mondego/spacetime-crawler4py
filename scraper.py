8c20f2083e4b5d755be9d4d5b1e61c59f649a3db
crista <lopes@ics.uci.edu>
2021-10-20 20:35:44 -0700

    Added documentation about the arguments of extract_next_links

--- scraper.py
+++ scraper.py
@@ -6,7 +6,14 @@ def scraper(url, resp):
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # Implementation requred.
+    # Implementation required.
+    # url: the URL that was used to get the page
+    # resp.url: the actual url of the page
+    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    # resp.error: when status is not 200, you can check the error here, if needed.
+    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+    #         resp.raw_response.url: the url, again
+    #         resp.raw_response.content: the content of the page!
     return list()
 
 def is_valid(url):
