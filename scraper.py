e19e34da72f75e065bccb49d8dc93473c478bcf8
Epsil-m <anemail@address.com>
2022-04-13 06:31:14 -0700

    basic extract implement for testing

--- scraper.py
+++ scraper.py
@@ -1,5 +1,10 @@
 import re
 from urllib.parse import urlparse
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+Blacklist = set()
+Visited = set()
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,17 +20,49 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    nextLinks = set()
+    global Blacklist
+    global Visited
+
+    if resp.status != 200:
+        print(resp.error)
+    
+    if(resp.status != 200 or url in BlackList or url in Visited):
+        Blacklist.add(url)
+        return ()
+
+    soup = BeautifulSoup(resp.raw_response.content, "html.parser")
+    for link in soup.find_all('a'):
+        href = link.attrs.get('href')
+
+        # If link is relative make absolute link
+        if urlparse(href).netloc == "":
+            href = urljoin(url, href)
+
+        if is_valid(href):
+            nextLinks.add(href)
+    # Add current url to list of visited urls so we don't end up visiting already visited links
+
+    parsed = urlparse(url)
+    Visited.add(parsed.scheme + '://' + parse.netloc + parsed.path)
+    return nextLinks
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
+    global Visited
+
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
+        if parsed.netloc not in set(["www.ics.uci.edu", "www.cs.uci.edu", "www.informatics.uci.edu", "www.stat.uci.edu", "www.today.uci.edu"]):
+            return False
+        if parsed.netloc == "www.today.uci.edu" and parsed.path != "/department/information_computer_sciences/":
+            return False
+        
+        if re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
@@ -33,7 +70,14 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
+            return False
+
+        if url in Visited:
+            return False
+
+        return True
+        
 
     except TypeError:
         print ("TypeError for ", parsed)
