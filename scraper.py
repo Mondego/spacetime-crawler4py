99cd87dd01e5c4bbde46897becf1e7fb894e09fe
AlexManuel1 <AlexMatamoros10@gmail.com>
2022-04-14 18:18:36 -0700

    modified scraper function and added tokenize_response, check_longest_page, computer_word_frequencies, and in_stop_words

--- scraper.py
+++ scraper.py
@@ -2,13 +2,57 @@ import re
 from urllib.parse import urlparse
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
+import nltk
+nltk.download('punkt')
 
 Blacklist = set()
 Visited = set()
+Stop_Words = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"}
+Longest_Page = ()
+Common_Words = {}
 
 def scraper(url, resp) -> list:
     links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    valid_links = [link for link in links if is_valid(link)]
+    if resp.status == 200:
+        word_token_list = tokenize_response(resp) #gather all tokens from webpage
+        check_longest_page(url, len(word_token_list)) #check if Longest_Page needs to be updated
+        compute_word_frequencies(word_token_list) #find frequencies of each token and insert into Common_Words
+        #print(Common_Words)
+        #print(Longest_Page)
+    return valid_links
+
+# get data from website and tokenize it taking out everything that isn't a word
+def tokenize_response(resp):
+    content = resp.raw_response.content
+    soup = BeautifulSoup(content, "html.parser")
+    tokens = nltk.tokenize.word_tokenize(soup.get_text()) # uses nltk to tokenize webpage
+    word_tokens = [t for t in tokens if not re.match('[\W]+', t)]
+    return word_tokens
+
+# assign new value to Longest_Page if current page is longer
+def check_longest_page(url, word_token_list_len):
+    global Longest_Page
+    if Longest_Page and Longest_Page[1] < word_token_list_len:
+        Longest_Page = (url, word_token_list_len)
+    elif not Longest_Page:
+        Longest_Page = (url, word_token_list_len)
+
+# increment word count in Common_Words for all words found on this page
+def compute_word_frequencies(tokenList):
+    # for loop which adds count to Common_Words dictionary 
+    global Common_Words
+    for token in tokenList:
+        if token in Common_Words:
+            Common_Words[token] += 1
+        elif token not in Common_Words and not in_stop_words(token): # checks to see if token is a stop word if it has not been added to Common_Words
+            Common_Words[token] = 1
+
+# returns true if a word is a stop word otherwise returns False
+def in_stop_words(token):
+    if token in Stop_Words:
+        return True
+    return False
 
 def extract_next_links(url, resp):
     # Implementation required.
