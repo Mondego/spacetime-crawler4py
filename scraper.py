edba70cd086264e0615830fac77a4333c3d7118a
Paolo Pedrigal <paolopedrigal@gmail.com>
2022-04-22 18:23:37 -0700

    Continued working on finishing assignment 2 requirements.

--- scraper.py
+++ scraper.py
@@ -2,57 +2,98 @@ import re
 from urllib.parse import urlparse
 from urllib.robotparser import RobotFileParser
 from bs4 import BeautifulSoup
-from utils.config import *
 from utils.response import Response
+
+# TODO: make sure to defragment the URLs, i.e. remove the fragment part. (COMPLETE??)
+# TODO: filter out urls that are not with the following domains .ics.uci.edu/, .cs.uci.edu/, .informatics.uci.edu/,
+                    # .stat.uci.edu/,today.uci.edu/department/information_computer_sciences/  (COMPLETE???)
+
+
+# TODO: crawl all pages with high textual information content - lecture 12
+# TODO: detect and avoid infinite traps - lecture 7
+# TODO: detect and avoid sets of similar pages with no information - lecture 12
+# TODO: store UNIQUE urls in frontier.py - lecture 11
+# TODO: Detect and avoid crawling very large files, especially if they have low information value - lecture 12
+# TODO: implement robotparser for robots.txt in extract_next_links (EC)
+
 
 def scraper(url: str, resp: Response) -> list:
     links = extract_next_links(url, resp)
     scraped = [link for link in links if is_valid(link)]
+    print(scraped)
     return scraped
 
 def extract_next_links(url: str, resp: Response):
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+    """"
+    Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
 
+    url: the URL that was used to get the page
+    resp.url: the actual url of the page
+    resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    resp.error: when status is not 200, you can check the error here, if needed.
+    resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+            resp.raw_response.url: the url, again
+            resp.raw_response.content: the content of the page!
+    """
     hyperlinks = list()
 
+    #which website to add to frontier and list
+    # check if the website has any data. if not then we skip
+    # check if website is similar to another website. if it is then we skip
+    # check if website has a lot of data and low information. if it is then skip. ***** how do we check this? *****
+
+    #crawler traps
+    #avoid calendars bc it causes infinite loop. other traps.
+
     if resp.status == 200:
-        soup = BeautifulSoup(resp.raw_response.content, "lxml")
-        for anchor in soup.find_all("a"):
-            if anchor.has_attr("href"):
-                hyperlinks.append(anchor["href"])
-    
+        if resp.raw_response.content != None:
+            soup = BeautifulSoup(resp.raw_response.content, "lxml")
+            for anchor in soup.find_all("a"):
+                if anchor.has_attr("href"):
+                    parse_href = urlparse(anchor["href"])
+                    hyperlinks.append(str(parse_href[0:5])) # defragments hyperlink reference
+
+    print(hyperlinks)   # DELETE THIS
+
     return hyperlinks
 
 def is_valid(url):
-    # Decide whether to crawl this url or not. 
-    # If you decide to crawl it, return True; otherwise return False.
-    # There are already some conditions that return False.
+    """
+    Decide whether to crawl this url or not.
+
+    Domains and paths to only consider valid:
+        *.ics.uci.edu/*
+        *.cs.uci.edu/*
+        *.informatics.uci.edu/*
+        *.stat.uci.edu/*
+        today.uci.edu/department/information_computer_sciences/*
+    """
 
     try:
         parsed = urlparse(url)
-        if parsed.scheme not in set(["http", "https"]):
-            return False
+        if parsed.scheme not in set(["http", "https"]): # if scheme does  not have http or https, the url is invalid (return False)
+            return False 
 
-        # if path does  not match with any of the below patterns
-        return not re.match(
+        # url is valid (set to True) if it doesn't have any of below file extensions in the path 
+        does_not_include_file_extension = not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|calendar"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
         
+        valid_domain = re.match(r".*\.ics\.uci\.edu|\.cs\.uci\.edu|\.informatics\.uci\.edu|\.stat\.uci\.edu|"
+            + r"today\.uci\.edu\/department\/information_computer_sciences", parsed.netloc.lower()) is not None
+        
+        return does_not_include_file_extension and valid_domain
 
     except TypeError:
         print ("TypeError for ", parsed)
         raise
+
+if __name__ == "__main__":
+    url_test = "https://www.ics.uci.edu/.pdf"
+    print(is_valid(url_test))
