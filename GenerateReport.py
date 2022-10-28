f051bc95a401fa5f8053fe07002274389277d53f
AnhVo3 <vophuonganh3@gmail.com>
2022-10-27 23:14:24 -0700

    Add Generate Response Class

---
+++ GenerateReport.py
@@ -0,0 +1,16 @@
+class GenerateReports:
+    def write_reports():
+        
+        pass
+
+    def __find_unique_pages(self):
+        pass
+
+    def __find_longest_page():
+        pass
+
+    def __find_common_words():
+        pass
+
+    def __find_subdomains():
+        pass
