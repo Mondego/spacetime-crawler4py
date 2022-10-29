98d9ade8e74b703ed43fc8dc8aae6bfd65ac569a
Arthur D Siu <51488740+arthurdsiu@users.noreply.github.com>
2022-10-28 17:43:54 -0700

    Added measures against query traps

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import re
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse, urljoin, urldefrag
+from urllib.parse import urlparse, urljoin, urldefrag, urlunparse
 
 #q1 - unique pages
 visitedPages = set()
@@ -17,6 +17,8 @@ subDomainCount = dict()
 stopWords = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours	ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"}            
 
 domains = ["ics.uci.edu", "cs.uci.edu", "informatics.uci.edu" ,"stat.uci.edu"]
+
+disallowQueriesDomains = ["swiki.ics.uci.edu"]
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -60,7 +62,13 @@ def extract_next_links(url, resp):
         href = link.get('href')
         href = urldefrag(href)[0] # assume we want to remove fragments
         href = urljoin(url, href) #join for relative URLS
-        parse = urlparse(href)
+
+        # if URL is blacklisted for queries, remove query string
+        parsed = urlparse(href)
+        if parsed.hostname in disallowQueriesDomains:
+            parsed._replace(query='') #remove query string
+            href = urlunparse(parsed)
+
         if is_valid(href) == True and href not in visitedPages:
             ret.append(href)
       
