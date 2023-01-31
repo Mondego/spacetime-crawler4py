d5286661a89264cf46af3916018e72e821856435
Ramanuj Sarkar <ramanuj.sarkar.ge@gmail.com>
2023-01-30 23:38:59 -0800

    Added remove_stopwords
    Added a function which removes stop words from the input. I may later fold this into another function.

--- tokenizer.py
+++ tokenizer.py
@@ -54,3 +54,21 @@ def compute_word_frequencies(token_list: [str]) -> {str: int}:
     for token in token_list:
         frequencies[token] += 1
     return frequencies
+
+
+def remove_stopwords(word_list: {str}):
+    # These are all the stop words that I feel are relevant.
+    stopwords = {'a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', 'aren',
+                 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', 'can',
+                 'cannot', 'could', 'couldn', 'd', 'did', 'didn', 'do', 'does', 'doesn', 'doing', 'don', 'down',
+                 'during', 'each', 'few', 'for', 'from', 'further', 'had', 'hadn', 'has', 'hasn', 'have', 'haven',
+                 'having', 'he', 'her', 'here', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+                 'into', 'is', 'isn', 'it', 'its', 'itself', 'let', 'll', 'm', 'me', 'more', 'most', 'mustn', 'my',
+                 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours',
+                 'ourselves', 'out', 'over', 'own', 're', 's', 'same', 'shan', 'she', 'should', 'shouldn', 'so', 'some',
+                 'such', 't', 'than', 'that', 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', 'these',
+                 'they', 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 've', 'very', 'was', 'wasn',
+                 'we', 'were', 'weren', 'what', 'when', 'where', 'which', 'while', 'who', 'whom', 'why', 'with',
+                 'would', 'wouldn', 'you', 'your', 'yours', 'yourself', 'yourselves'}
+    # This is the list which doesn't have stop words.
+    word_list = word_list - stopwords
