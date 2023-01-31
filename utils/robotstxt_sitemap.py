b8fe41b20b196318b08382b6b9682f664e3703d2
Hamin Park <haminp@uci.edu>
2023-01-30 22:59:25 -0800

    finish writing get_sitemap_urls

--- utils/robotstxt_sitemap.py
+++ utils/robotstxt_sitemap.py
@@ -1,3 +1,4 @@
+import time
 from response import Response
 from config import Config
 from logging import Logger
@@ -9,8 +10,19 @@ def get_sitemap_urls(seed_urls: list[str], config: Config, logger: Logger=None) 
     sitemap_urls = list()
     for url in seed_urls:
         response = download(url, config, logger)
-        sitemap_urls += _extract_sitemap_urls(response)
+        if response.status == 200:
+            sitemap_urls += _extract_sitemap_urls(response)
+        time.sleep(config.time_delay)
     return sitemap_urls
 
 def _extract_sitemap_urls(resp: Response) -> list[str]:
-    pass
+    '''Return all the sitemap urls from the response'''
+    extracted_urls = list()
+    if resp.raw_response is None:
+        # shouldn't happen, but just in case
+        return extracted_urls
+
+    for line in resp.raw_response.split():
+        if line.startswith('Sitemap'):
+            extracted_urls.append(line.split()[1])
+    return extracted_urls
