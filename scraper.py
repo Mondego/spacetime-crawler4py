0a1ca0604b29a61a69a9a95efdafab03cdaf4e88
Kiran Maya Sheikh <kmsheikh@circinus-26.ics.uci.edu>
2023-02-02 00:14:07 -0800

    Develop scraper.py, use BeautifulSoup library to parse html hyperlinks.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,21 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    
+    hyperlink_list = []        # initialize empty list
+    
+    if resp.status == 200:
+        print("Status code: {}, success.".format(resp.status))   # PRINT CHECK
+        page_soup = BeautifulSoup(resp.raw_response.content, "html.parser")         # Create the BeautifulSoup object
+        # hyperlinks = page_soup.find_all(href=True)   # find all hyperlinks?
+        
+        for link in page_soup.find_all("a"): 
+            print(link.get('href'))
+
+    else:
+        print("Status code: {}, error.".format(resp.status))   # PRINT CHECK
+
+    return hyperlink_list
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
