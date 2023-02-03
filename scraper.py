869b57e976f6ece04b446496cfec15e5e88d4de7
Brian Caballero <briancaballero7@gmail.com>
2023-02-03 15:20:45 -0800

    Deliverable Vars & is_valid update

--- scraper.py
+++ scraper.py
@@ -1,5 +1,14 @@
 import re
+import nltk
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+
+
+UniqueUrlSet = set() #Set of URLs checked already and for deliverable Q1
+PageWithMostWords = "" #Deliverable Q2
+MostCommonWordsList = list() #Deliverable Q3
+SubDomainsFoundDict = dict() #Deliverable Q4
+
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -16,30 +25,42 @@ def extract_next_links(url, resp):
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
     
-    if(resp.status == 200): #Check if we got onto the page
-
-        pass
+    if(resp.status == 200 and resp.raw_response.content != None): #Check if we got onto the page and if it has content
+        _soupHtml = BeautifulSoup(resp.raw_response.content, 'html.parser')
+        _urlTokenList = nltk.tokenize(_soupHtml.getText())
     else:
         print("resp.status code: ", resp.status, "\nError of: ", resp.error)
+        return list()
+
+
+    
     return list()
 
 def is_valid(url):
+    global UniqueUrlSet
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
     try:
         parsed = urlparse(url)
+        if parsed.hostname == None or parsed.netloc == None: #Check if hostname is not None
+            return False
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+        if any(_domainName in parsed.hostname for _domainName in [".ics.uci.edu","cs.uci.edu",".informatics.uci.edu",".stat.uci.edu"]): #Only check for certain domains (Assignemnt)
+            if not re.match(
+                r".*\.(css|js|bmp|gif|jpe?g|ico"
+                + r"|png|tiff?|mid|mp2|mp3|mp4"
+                + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+                + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+                + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+                + r"|epub|dll|cnf|tgz|sha1"
+                + r"|thmx|mso|arff|rtf|jar|csv"
+                + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
+                if url not in UniqueUrlSet: 
+                    UniqueUrlSet.add(url)
+                    return True
+        return False
 
     except TypeError:
         print ("TypeError for ", parsed)
