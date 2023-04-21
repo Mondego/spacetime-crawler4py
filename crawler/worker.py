f23cbb0d3989f489d9b731c43e40ce37f8183f33
aalkhuna <aalkhuna@uci.edu>
2023-04-21 09:43:27 -0700

    Scraper converted to a class.

--- crawler/worker.py
+++ crawler/worker.py
@@ -3,7 +3,7 @@ from threading import Thread
 from inspect import getsource
 from utils.download import download
 from utils import get_logger
-import scraper
+from scraper import Scraper
 import time
 
 
@@ -27,7 +27,8 @@ class Worker(Thread):
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
-            scraped_urls = scraper.scraper(tbd_url, resp)
+            scraper_instance = Scraper()
+            scraped_urls = scraper_instance.scraper(tbd_url, resp)
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
--- scraper.py
+++ scraper.py
@@ -1,21 +1,26 @@
 import re
 from urllib.parse import urlparse
 
-def scraper(url, resp):
-    links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+class Scraper:
 
-def extract_next_links(url, resp):
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    def __init__(self):
+        self.scraped_urls = []
+    
+    def scraper(self, url, resp):
+        links = extract_next_links(url, resp)
+        return [link for link in links if is_valid(link)]
+
+    def extract_next_links(url, resp):
+        # Implementation required.
+        # url: the URL that was used to get the page
+        # resp.url: the actual url of the page
+        # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+        # resp.error: when status is not 200, you can check the error here, if needed.
+        # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+        #         resp.raw_response.url: the url, again
+        #         resp.raw_response.content: the content of the page!
+        # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+        return list()
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -30,7 +35,7 @@ def is_valid(url):
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+                + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
