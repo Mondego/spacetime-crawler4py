4d38bf079a2d824d47f2b61f9ac944a788ba6661
Tuan Nguyen <tuanvnguyen556@gmail.com>
2023-04-23 19:02:43 -0700

    adding fragment remover function

--- unique.py
+++ unique.py
@@ -1,7 +1,16 @@
 class Unique:
     def __init__(url: str):
-        url_list = []
+        """
+        Unique objects helps determine unique urls,
+        ignoring the fragmented parts of the url, and
+        counts the number of unique urls
+        """
+        url_list = [] #
         unique_url_counter = 0
 
     def url_to_unique(url:str):
-        pass
+        no_fragment = True
+        for c in url:
+            if no_fragment:
+                if 35 == ord(c):
+                    no_fragment = False
