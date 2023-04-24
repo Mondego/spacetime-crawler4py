3c50a6ce89d5c37cc71d5edab3672bfa474ac3ba
Tuan Nguyen <tuanvnguyen556@gmail.com>
2023-04-23 18:51:15 -0700

    pulled from master

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import re
 from urllib.parse import urlparse
-
+from bs4 import BeautifulSoup
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
@@ -15,7 +15,15 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    
+    extracted_links = set()
+    if resp.status != 200:
+        print(url)
+    soup = BeautifulSoup(resp.raw_response.content, "html.parser")
+    for link in soup.find_all('a'):
+        cur_url = link.get('href')
+        extracted_links.add(cur_url[:cur_url.find('#')])
+    return list(extracted_links)
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
