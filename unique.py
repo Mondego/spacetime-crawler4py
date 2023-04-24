2ac02bc6b6eee92d72352d4028a9b28548508636
Tuan Nguyen <tuanvnguyen556@gmail.com>
2023-04-23 19:08:26 -0700

    need comments for url_to_unique

--- unique.py
+++ unique.py
@@ -10,7 +10,11 @@ class Unique:
 
     def url_to_unique(url:str):
         no_fragment = True
+        new_url = ""
         for c in url:
             if no_fragment:
                 if 35 == ord(c):
                     no_fragment = False
+                else:
+                    new_url += c
+        return new_url
