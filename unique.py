c6dc449145449b04eca6524941b9c3630af73eab
Tuan Nguyen <tuanvnguyen556@gmail.com>
2023-04-23 19:33:29 -0700

    done with unique counter

--- unique.py
+++ unique.py
@@ -1,24 +1,27 @@
+
 class Unique:
     """
     Unique objects helps determine unique urls,
     ignoring the fragmented parts of the url, and
-    counts the number of unique urls
+    counts the number of unique urls.
     """
     def __init__(self):
         """
-        Initializer takes in a url 
+        Initializer will set up a url set container
+        that stores all the unique urls and initialize the
+        unique url counter to 0.
         """
-        url_list = set() #contains all the unique urls
-        url_counter = 0 #counts how many unique urls are crawled to
+        self.url_set = set() #contains all the unique urls
+        self.url_counter = 0 #counts how many unique urls are crawled to
 
     def remove_fragment(self, url:str):
         """
-        remove_fragment loops through a given url and
+        The function remove_fragment loops through a given url and
         will remove the fragment that is at the end of the
         url. This function will run O(N), as it goes through every
         character of the url.
         """
-        no_fragment = True #
+        no_fragment = True #boolean value that switches if a fragment is reached
         new_url = "" #url to return as it will be unique
         for c in url: #loops through url O(N) time
             if no_fragment:
@@ -26,11 +29,15 @@ class Unique:
                     no_fragment = False #makes it false to prevent adding fragment characters
                 else:
                     new_url += c #increment every character before the fragment
-
-        url_list.add(new_url) #adds the unique url to the set
+       
+        if new_url not in self.url_set: #increments the unique url counter if not in container
+            self.url_counter += 1
+        
+        self.url_set.add(new_url) #adds the unique url to the set
+        
         return new_url
 
 
+
 if __name__ == "__main__":
-    store = Unique()
-    print(store.remove_fragment("asefdsfd#dasfsdfa"))
+    pass
