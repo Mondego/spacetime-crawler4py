8cd442f797f185bb4ea7c4be56bde1b39dd92eaf
Adit Mohindra <aditmohindra@gmail.com>
2023-10-26 06:15:07 -0700

    glizzy

--- scraper.py
+++ scraper.py
@@ -5,44 +5,122 @@ from bs4 import BeautifulSoup
 from collections import Counter
 from difflib import SequenceMatcher
 
-# Seed URL: http://www.ics.uci.edu
+seed = "https://ics.uci.edu/" 
+# seed = "https://sami.ics.uci.edu/research.html"
+
+# linkSet will transform list of links into a set to remove duplicates
+linkSet = set()
+
+# pagewordCounts dictionary will hold url and word count
+pageWordCounts = {}
+
+# subdomainCounts dictionary will hold subdomains and their frequency
+subdomainCounts = {}
+
+# wordCounter will hold number of times a certain word is read
+wordCounter = Counter()
+
+# list of stopwords
+stopWords = stopwords = set([
+    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", 
+    "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", 
+    "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", 
+    "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", 
+    "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", 
+    "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", 
+    "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", 
+    "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", 
+    "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", 
+    "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", 
+    "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", 
+    "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", 
+    "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", 
+    "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", 
+    "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", 
+    "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", 
+    "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"
+])
+
 
 def scraper(url, resp):
-    # list of valid links
-    links = extract_next_links(url, resp)
+    if is_valid(url):
+        print("Testing URL: " , url)
+        try:
+            resp = requests.get(url)
+        except:
+            print("Timeout")
+
+        # Store number of unique pages
+        links = extract_next_links(url, resp)
+
+        if links is not None:
+            for link in links:
+                if link not in linkSet:
+                    linkSet.add(link)
+        else:
+            print("No more links here! Moving on...")
+
+        links.remove(url)
+
+        print("Extracted Links:")
+        if(links is not None):
+            linkSet.update(links)
+            # Find number of unique pages
+            uniquePages = len(linkSet)
+            print("Number of Unique Pages: ", uniquePages)
+        
+        # Store word count for the current URL
+        content = resp.text
+        pageWordCounts[url] = count_words(content)
+
+        # Update wordCounter for each tokenized word, not including stop words
+        tokens = tokenize(content)
+        for word in tokens:
+            if word not in stopWords:
+                wordCounter[word] += 1
+
+        parsed_url = urlparse(url)
+        if parsed_url.netloc.endswith('ics.uci.edu'):
+            
+            # Extract the subdomain part
+            subdomain = parsed_url.netloc.rsplit('.', 2)[0]
+
+            if subdomain == 'ics':
+                subdomain = parsed_url.netloc.rsplit('.', 3)[1]
+            
+            # Increment count for the subdomain or initialize it if it doesn't exists
+            subdomainCounts[subdomain] = subdomainCounts.get(subdomain, 0) + 1
+
+        # if links is not None:
+            # for link in links:
+        # if linkSet is not None:
+        #     for link in linkSet:
+        #         print(link)
+    else:
+        print(url, " is not a valid URL for crawling.")
 
 
 
+    # Find the url of the longest page in terms of words count
+    if pageWordCounts:
+        longest_page_url = max(pageWordCounts, key=pageWordCounts.get)
+        print("Longest page URL:", longest_page_url)
+        print("Number of words:", pageWordCounts[longest_page_url])
 
+    # Get the 50 most common words
+    most_common_words = wordCounter.most_common(50)
+    print("50 most common words:", most_common_words)
 
-
-
+    # Print out the counts for each subdomain
+    sorted_subdomains = sorted(subdomainCounts.items(), key=lambda x: x[0])  # Sort by subdomain name
+    for subdomain, count in sorted_subdomains:
+        print(f"http://{subdomain}.ics.uci.edu, {count}")
 
     # returns a list of links
-    return [link for link in links if is_valid(link)]
-
-    # NEED TO
-    # parse the web response, 'resp', extract information from here answering deliverable questions
-    # return list of URL's scrapped from that page
-    #   return only valid URL's
-    #   defragment URL's (remove fragment)
-    #   use library (BeautifulSoup, lxml)
-
+    return [link for link in linkSet if is_valid(link)]
+    # return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status_code: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again    
-    #         resp.raw_response.content: the content of the page! (NOT CORRECT VARIABLE)
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-
-    # retrieving final page URL after redirects - keep this Greg for testing/matching after implementation
-    # actual_url = resp.url
-
     link_list = []
 
 
@@ -57,6 +135,7 @@ def extract_next_links(url, resp):
 
             soup = BeautifulSoup(resp.text, 'html.parser')
 
+            # this code is mid i think
             # we want to eliminate the possibility of a 404 page which doesnt return 
             # an error 404 code, such as http://cs.uci.edu/page
             # title_tag = soup.find("title")
@@ -132,62 +211,6 @@ def is_valid(url):
         print("TypeError for ", parsed)
         raise
 
-# DRIVER CODE
-
-test_urls = [
-    # These are all for validity checker
-    # "https://www.ics.uci.edu/page",
-    # "http://cs.uci.edu/page",
-    # "https://informatics.uci.edu/page",
-    # "https://stat.uci.edu/page",
-    # "https://www.google.com/page",
-    # "ftp://invalid-url.com/ftp-page",
-    # "https://www.linkedin.com/feed/",
-    # "https://drive.google.com/drive/u/0/my-drive",
-    # "https://www.youtube.com/watch?v=_ITiwPMUzho&ab_channel=LofiGhostie",
-    # "https://www.youtube.com/watch?v=TUEju_i3oWE&ab_channel=Insomniac",
-    # "https://github.com/gregkhanoyan/IR23F-A2-G33#things-to-keep-in-mind",
-    # "https://canvas.eee.uci.edu/courses/58552/assignments/1243743",
-    # These are actual links that can be crawled
-    # "https://ics.uci.edu/academics/undergraduate-academic-advising/",
-    # "https://ics.uci.edu/academics/undergraduate-academic-advising/change-of-major/",
-    # "https://grape.ics.uci.edu/wiki/public/wiki/cs122b-2019-winter",
-
-    # "http://www.ics.uci.edu",
-    "https://sami.ics.uci.edu/"
-
-]
-
-# linkSet transforms list of links into a set to remove duplicates
-linkSet = set()
-# pagewordCounts dictionary holds url and word count
-pageWordCounts = {}
-# subdomainCounts dictionary hold subdomains and it's frequency
-subdomainCounts = {}
-# wordCounter will hold number of times a certain word is read
-wordCounter = Counter()
-
-stopWords = stopwords = set([
-    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", 
-    "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", 
-    "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", 
-    "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", 
-    "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", 
-    "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", 
-    "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", 
-    "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", 
-    "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", 
-    "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", 
-    "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", 
-    "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", 
-    "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", 
-    "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", 
-    "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", 
-    "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", 
-    "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"
-])
-
-
 def tokenize(content):
     # Strip HTML markup
     soup = BeautifulSoup(content, 'html.parser')
@@ -214,84 +237,117 @@ def count_words(content):
     words = re.findall(r'\w+', newContent)
     return len(words)
 
-# move this all to scraper func
-for url in test_urls:
-    if is_valid(url):
-        print("Testing URL: " , url)
-        try:
-            resp = requests.get(url)
-        except:
-            print("Timeout")
-        # Store number of unique pages
-        links = extract_next_links(url, resp)
-
-        if links is not None:
-            for link in links:
-                if link not in linkSet:
-                    test_urls.append(link)
-        else:
-            print("No more links here! Moving on...")
-
-        test_urls.remove(url)
-
-        print("Extracted Links:")
-        if(links is not None):
-            linkSet.update(links)
-            # Find number of unique pages
-            uniquePages = len(linkSet)
-            print("Number of Unique Pages: ", uniquePages)
 
 
-        # Store word count for the current URL
-        content = resp.text
-        pageWordCounts[url] = count_words(content)
+resp = requests.get(seed)
+urlsss = scraper(seed, resp)
 
-        # Update wordCounter for each tokenized word, not including stop words
-        tokens = tokenize(content)
-        for word in tokens:
-            if word not in stopWords:
-                wordCounter[word] += 1
+print("URL's Scraped From Seed")
+for i in urlsss:
+    print(i)
 
-        parsed_url = urlparse(url)
-        if parsed_url.netloc.endswith('ics.uci.edu'):
-            # Extract the subdomain part
-            subdomain = parsed_url.netloc.rsplit('.', 2)[0]
 
-            if subdomain == 'ics':
-                subdomain = parsed_url.netloc.rsplit('.', 3)[1]
+# ------------------------------------------------------------------- OLD DRIVER CODE - KEEP JUST IN CASE----------------------------------------------------------------------------
+
+# test_urls = [
+#     # These are all for validity checker
+#     # "https://www.ics.uci.edu/page",
+#     # "http://cs.uci.edu/page",
+#     # "https://informatics.uci.edu/page",
+#     # "https://stat.uci.edu/page",
+#     # "https://www.google.com/page",
+#     # "ftp://invalid-url.com/ftp-page",
+#     # "https://www.linkedin.com/feed/",
+#     # "https://drive.google.com/drive/u/0/my-drive",
+#     # "https://www.youtube.com/watch?v=_ITiwPMUzho&ab_channel=LofiGhostie",
+#     # "https://www.youtube.com/watch?v=TUEju_i3oWE&ab_channel=Insomniac",
+#     # "https://github.com/gregkhanoyan/IR23F-A2-G33#things-to-keep-in-mind",
+#     # "https://canvas.eee.uci.edu/courses/58552/assignments/1243743",
+#     # These are actual links that can be crawled
+#     # "https://ics.uci.edu/academics/undergraduate-academic-advising/",
+#     # "https://ics.uci.edu/academics/undergraduate-academic-advising/change-of-major/",
+#     # "https://grape.ics.uci.edu/wiki/public/wiki/cs122b-2019-winter",
+
+#     # "http://www.ics.uci.edu",
+#     "https://sami.ics.uci.edu/"
+
+# ]
+
+# for url in test_urls:
+#     if is_valid(url):
+#         print("Testing URL: " , url)
+#         try:
+#             resp = requests.get(url)
+#         except:
+#             print("Timeout")
+#         # Store number of unique pages
+#         links = extract_next_links(url, resp)
+
+#         if links is not None:
+#             for link in links:
+#                 if link not in linkSet:
+#                     test_urls.append(link)
+#         else:
+#             print("No more links here! Moving on...")
+
+#         test_urls.remove(url)
+
+#         print("Extracted Links:")
+#         if(links is not None):
+#             linkSet.update(links)
+#             # Find number of unique pages
+#             uniquePages = len(linkSet)
+#             print("Number of Unique Pages: ", uniquePages)
+
+
+#         # Store word count for the current URL
+#         content = resp.text
+#         pageWordCounts[url] = count_words(content)
+
+#         # Update wordCounter for each tokenized word, not including stop words
+#         tokens = tokenize(content)
+#         for word in tokens:
+#             if word not in stopWords:
+#                 wordCounter[word] += 1
+
+#         parsed_url = urlparse(url)
+#         if parsed_url.netloc.endswith('ics.uci.edu'):
+#             # Extract the subdomain part
+#             subdomain = parsed_url.netloc.rsplit('.', 2)[0]
+
+#             if subdomain == 'ics':
+#                 subdomain = parsed_url.netloc.rsplit('.', 3)[1]
             
-            # Increment count for the subdomain or initialize it if it doesn't exists
-            subdomainCounts[subdomain] = subdomainCounts.get(subdomain, 0) + 1
+#             # Increment count for the subdomain or initialize it if it doesn't exists
+#             subdomainCounts[subdomain] = subdomainCounts.get(subdomain, 0) + 1
 
-        # if links is not None:
-            # for link in links:
-        # if linkSet is not None:
-        #     for link in linkSet:
-        #         print(link)
-    else:
-        print(url, " is not a valid URL for crawling.")
+#         # if links is not None:
+#             # for link in links:
+#         # if linkSet is not None:
+#         #     for link in linkSet:
+#         #         print(link)
+#     else:
+#         print(url, " is not a valid URL for crawling.")
 
 
 
-# Find the url of the longest page in terms of words count
-if pageWordCounts:
-    longest_page_url = max(pageWordCounts, key=pageWordCounts.get)
-    print("Longest page URL:", longest_page_url)
-    print("Number of words:", pageWordCounts[longest_page_url])
+# # Find the url of the longest page in terms of words count
+# if pageWordCounts:
+#     longest_page_url = max(pageWordCounts, key=pageWordCounts.get)
+#     print("Longest page URL:", longest_page_url)
+#     print("Number of words:", pageWordCounts[longest_page_url])
 
-# Get the 50 most common words
-most_common_words = wordCounter.most_common(50)
-print("50 most common words:", most_common_words)
+# # Get the 50 most common words
+# most_common_words = wordCounter.most_common(50)
+# print("50 most common words:", most_common_words)
 
-# Print out the counts for each subdomain
-sorted_subdomains = sorted(subdomainCounts.items(), key=lambda x: x[0])  # Sort by subdomain name
-for subdomain, count in sorted_subdomains:
-    print(f"http://{subdomain}.ics.uci.edu, {count}")
+# # Print out the counts for each subdomain
+# sorted_subdomains = sorted(subdomainCounts.items(), key=lambda x: x[0])  # Sort by subdomain name
+# for subdomain, count in sorted_subdomains:
+#     print(f"http://{subdomain}.ics.uci.edu, {count}")
 
 # for url in test_urls:
 #     if is_valid(url):
 #         print(f"{url} is a valid URL for crawling.")
 #     else:
 #         print(f"{url} is not a valid URL for crawling.")
-
-
